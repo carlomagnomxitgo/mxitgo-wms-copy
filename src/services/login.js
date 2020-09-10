c387d241d5c630e9a72c635cf1a9f89f13d8a9cc
Carlomagno Lopez <clopez@mxitgo.com>
2020-09-10 16:26:21 -0500

    Update login.js

--- src/services/login.js
+++ src/services/login.js
@@ -12,6 +12,6 @@ export async function getFakeCaptcha(mobile) {
 export async function getDataUserByEmail(email) {
     return request(`${ANT_DESIGN_PRO_TARGET}/user/email?email=${email.email}`, {
         method: 'GET',
-        headers: { 'Authorization': email.Authorization },
+        // headers: { 'Authorization': email.Authorization },
     });
 }
