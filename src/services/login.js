55cd6a544435baa78178575400321eed355257f6
Carlomagno Lopez <clopez@mxitgo.com>
2020-09-23 14:40:29 -0500

    reverse methods

--- src/services/login.js
+++ src/services/login.js
@@ -17,7 +17,7 @@ export async function getDataUserByEmail(email) {
             'Access-Control-Allow-Origin': 'https://master.d33oxsmkbrwh67.amplifyapp.com',
             'Access-Control-Allow-Headers': 'X-Forwarded-For',
             'x-api-key': 'qvWmpGYLQl6KNPdJqz40SavJ7EjyFPqy8CG4rz71',
-            'Access-Control-Allow-Methods': 'OPTIONS,GET',
+            'Access-Control-Allow-Methods': 'GET,OPTIONS',
           }
         // headers: { 
         //     // 'Authorization': email.Authorization,
