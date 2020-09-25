0b274cf120c6120787c80ea409379d46ecfce919
Carlomagno Lopez <clopez@mxitgo.com>
2020-09-24 19:41:18 -0500

    Update login.js

--- src/services/login.js
+++ src/services/login.js
@@ -14,18 +14,22 @@ export async function getDataUserByEmail(email) {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'https://master.d33oxsmkbrwh67.amplifyapp.com',
-            'Access-Control-Allow-Headers': 'X-Forwarded-For',
+
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "https://master.d33oxsmkbrwh67.amplifyapp.com",
+            "Access-Control-Allow-Methods": "OPTIONS,GET",
+            // 'Access-Control-Allow-Origin': 'https://master.d33oxsmkbrwh67.amplifyapp.com',
+            // 'Access-Control-Allow-Headers': 'X-Forwarded-For',
             'x-api-key': 'qvWmpGYLQl6KNPdJqz40SavJ7EjyFPqy8CG4rz71',
-            'Access-Control-Allow-Methods': 'GET,OPTIONS',
-          }
+            // 'Access-Control-Allow-Methods': 'GET,OPTIONS',
+        }
         // headers: { 
         //     // 'Authorization': email.Authorization,
         //     // "Access-Control-Allow-Origin": "*",
         //     // "Access-Control-Allow-Headers": "X-Forwarded-For",
         //     // "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
         //     "x-api-key":"qvWmpGYLQl6KNPdJqz40SavJ7EjyFPqy8CG4rz71"
-        
+
         // },
     });
 }
