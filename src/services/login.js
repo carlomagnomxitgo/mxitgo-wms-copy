e7f7414e26e4b5d791e2a703e53fd3005ccd235f
Carlomagno Lopez <clopez@mxitgo.com>
2020-09-25 17:54:19 -0500

    Update login.js

--- src/services/login.js
+++ src/services/login.js
@@ -15,22 +15,8 @@ export async function getDataUserByEmail(email) {
         headers: {
             'Content-Type': 'application/json',
             'Authorization': email.Authorization,
-            // "Access-Control-Allow-Headers": "Content-Type",
-            // "Access-Control-Allow-Origin": "https://master.d33oxsmkbrwh67.amplifyapp.com",
-            // "Access-Control-Allow-Methods": "OPTIONS,GET",
-            // 'Access-Control-Allow-Origin': 'https://master.d33oxsmkbrwh67.amplifyapp.com',
-            // 'Access-Control-Allow-Headers': 'X-Forwarded-For',
             'x-api-key': 'qvWmpGYLQl6KNPdJqz40SavJ7EjyFPqy8CG4rz71',
-            // 'Access-Control-Allow-Methods': 'GET,OPTIONS',
         }
-        // headers: { 
-        //     // 'Authorization': email.Authorization,
-        //     // "Access-Control-Allow-Origin": "*",
-        //     // "Access-Control-Allow-Headers": "X-Forwarded-For",
-        //     // "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE",
-        //     "x-api-key":"qvWmpGYLQl6KNPdJqz40SavJ7EjyFPqy8CG4rz71"
-
-        // },
     });
 }
 
