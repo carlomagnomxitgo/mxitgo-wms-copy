ecc19812574106d56aea987e0e72b99bbbbdf388
CarlomagnoLopez <36904283+CarlomagnoLopez@users.noreply.github.com>
2020-11-05 12:32:04 -0600

    testing avatar without img

--- src/pages/generalComponents/UploadAvatar.jsx
+++ src/pages/generalComponents/UploadAvatar.jsx
@@ -1,25 +1,28 @@
 import React, { PureComponent } from 'react';
 import { _ } from 'lodash';
-import { Upload, Button, Icon} from 'antd';
+import { Upload, Button, Icon } from 'antd';
 import AvatarAccount from './AvatarAccount';
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import Styles from './StylesGeneral.css';
 
-export default class UploadAvatar extends PureComponent{
-  render(){
-    return(  
+export default class UploadAvatar extends PureComponent {
+  render() {
+    return (
       <div>
         <Upload
           onChange={this.props.changeImagen}
           customRequest={this.props.customRequest}
           showUploadList={false}
         >
-          <AvatarAccount dataImagen={this.props.stateImage}/>
+          {/* <AvatarAccount dataImagen={this.props.stateImage}/> */}
+          <Avatar style={{ backgroundColor: color, verticalAlign: 'middle' }} size="large">
+            user
+          </Avatar>
           <Button className={Styles.avatar}>
-            <Icon type="upload"/><FormattedMessage id="accountSettings.label.upload-image"/>
+            <Icon type="upload" /><FormattedMessage id="accountSettings.label.upload-image" />
           </Button>
         </Upload>
-      </div>  
+      </div>
     );
   }
 }
