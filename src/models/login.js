dda7c04bc6320e1cfa5c47e16b16422020cff12b
Carlos Gomez <63121784+carlos-gomez-v@users.noreply.github.com>
2020-11-10 23:18:43 -0600

    change the values in sessionActive

--- src/models/login.js
+++ src/models/login.js
@@ -53,7 +53,7 @@ const Model = {
                 localStorage.setItem('familyName', response.family_name);
                 localStorage.setItem('email', response.email);
                 localStorage.setItem('isRemembered', payload[1]);
-                localStorage.setItem('sessionActive', localStorage.getItem('CognitoIdentityServiceProvider.66vntbnp4mpgn1o1p50pqd43kl.e1451cee-0f07-4784-8395-cc0ac548913f.accessToken'));
+                localStorage.setItem('sessionActive', localStorage.getItem('CognitoIdentityServiceProvider.25h6ahb7sda3lvk1qs8v5u0ol0.c14be52e-be78-4949-a8c2-d815fe62b088.accessToken'));
 
                 window.location.href = '/dashboard';
             }
